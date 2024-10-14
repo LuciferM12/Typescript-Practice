cf2e1140916b450aa789724563effaf023dddbe9
LuciferM121 <rojasrodriguezmartinomar@gmail.com>
2024-10-13 23:38:39 -0600

    add annotating

---
+++ annotating.ts
@@ -0,0 +1,13 @@
+// let myVar: type = value
+//Strings
+let myName: string = "LuciferM12";
+myName = "Another name";
+console.log(myName);
+
+//Numbers
+let favNumber: number = 7;
+favNumber += 2;
+console.log(favNumber)
+
+//Boolean
+let tsHard: boolean = false;
