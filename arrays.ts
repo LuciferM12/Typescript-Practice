64cf68fb6993b1dfb7b37ed44ab9df1365b65116
LuciferM121 <rojasrodriguezmartinomar@gmail.com>
2024-10-18 19:33:01 -0600

    add arrays

---
+++ arrays.ts
@@ -0,0 +1,13 @@
+//[] notation
+
+const numbers: number[] = [1, 2, 3, 4];
+const names: Array<string> = ["Alice", "Bob", "Charlie"];
+
+const items: string[] = [];
+items.push('Keyboard');
+
+//nested arrays
+const matrix: number[][] = [
+    [1, 2],
+    [3, 4]
+];
